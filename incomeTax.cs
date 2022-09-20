21f0f0a02381f92b47b9c921f18cf53d0e4f37c0
Samuel Ramachandran <67542651+samuelramachandran@users.noreply.github.com>
2022-09-20 15:52:07 +0530

    Task 3

---
+++ incomeTax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Vat
+    {
+        public void calculateTax(double n1)
+        {
+            Console.WriteLine("The Vat to pay Rs.{0}", n1 * 0.2);
+        }
+    }
+
+    class Gst : Vat
+    {
+        public void calculateTax(double n1)
+        {
+            Console.WriteLine("GST to pay Rs.{0}", n1 * 0.12);
+        }
+    }
+    internal class incomeTax:Gst
+    {
+        public static void Main()
+        {
+            Vat obj = new Vat();
+            obj.calculateTax(1232.2);
+            incomeTax obj1 = new incomeTax();
+            obj1.calculateTax(324.34);
+
+        }
+    }
+}
+
---
+++ validationPoly.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp
+{
+    public class validationPoly
+    {
+
+       
+        public void validate(string eid, string pwd)
+        {
+            Console.WriteLine("Email and password validated");
+        }
+
+        public void validate(string mid, int pin)
+        {
+            Console.WriteLine("Member id and pin validated");
+        }
+
+        public void validate(double n1, double n2)
+        {
+            Console.WriteLine("Mobile number and pin validated");
+        }
+
+        public static void Main()
+        {
+            validationPoly obj = new validationPoly();
+            obj.validate("info@sam.com", "!@#$%^");
+            obj.validate("ABC-001",0987);
+            obj.validate(9876543210, 0987);
+        }
+    }
+}
+
